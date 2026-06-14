0e828d1478ba8459fe100a2f63919fa84c1c82a5
arduinohatesme <awmcmillan128@gmail.com>
2026-06-14 15:55:17 -0500

    Change base path from '/arduinohatesme/' to '/portfolio/'

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
-  base: "/arduinohatesme/",
+  base: "/portfolio/",
   build: {
     outDir: "./dist",
     emptyOutDir: true,
