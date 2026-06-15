ba8315eb34ecbdaf87ac9356210fdee6275b219b
arduinohatesme <awmcmillan128@gmail.com>
2026-06-15 16:10:04 -0500

    Get pinned repos dynamically

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "../style.css";
 import { cycleText } from "./cycle-text.ts";
+import { getPinnedRepos } from "./get-pinned.ts";
 import RepoCard from "./repo-card.ts";
 
+const repo_cards: HTMLCollectionOf<Element> | null =
+  document.getElementsByTagName("repo-card");
 const head_cycle_element: HTMLElement | null =
   document.querySelector("#cycling-header");
 
@@ -29,4 +32,28 @@ if (head_cycle_element !== null) {
 
 if (!customElements.get("repo-card")) {
   customElements.define("repo-card", RepoCard);
+}
+
+const repos: Array<Object> = await getPinnedRepos();
+
+for (const [i, card] of Array.from(repo_cards).entries()) {
+  let repo = repos[i];
+  if (repo === undefined) {
+    card.remove();
+    continue;
+  }
+  if (
+    "author" in repo &&
+    "name" in repo &&
+    "description" in repo &&
+    "stars" in repo &&
+    "desc" in card &&
+    "repo" in card &&
+    "stars" in card
+  ) {
+    card.repo = `${repo.author}/${repo.name}`;
+    card.desc = repo.description;
+    card.stars = repo.stars;
+    (card as any).enabled = "enabled";
+  }
 }
