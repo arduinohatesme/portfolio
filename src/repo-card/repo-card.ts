fe7c1ab72b7fce8d82e1339a9c5cbd4f0abf3b2b
arduinohatesme <awmcmillan128@gmail.com>
2026-07-21 22:17:31 -0500

    code: Refactor repo card

--- src/repo-card/repo-card.ts
+++ src/repo-card/repo-card.ts
@@ -5,68 +5,12 @@ export default class RepoCard extends HTMLElement {
     return ["repo", "desc", "stars", "enabled"];
   }
 
-  connectedCallback() {
+  mkInnerHTML() {
     const repo = this.getAttribute("repo") || "Repo Name";
     const desc = this.getAttribute("desc") || "Description";
-    const stars = this.getAttribute("stars") || "0";
-    const enabled = this.getAttribute("enabled") || false;
-
-    this.innerHTML = `
-      <a class="repo-link" href="https://git.arduinohates.me/${repo}" target="_blank" class=${enabled ? "" : ".disabled-link"}>
-        <h3 class="repo-head">
-          ${repo}
-        </h3>
-        <div class="repo-desc-stars">
-          <p class="repo-desc">${desc}</p>
-          <p class="repo-stars">${stars} ${stars === "1" ? "Star" : "Stars"}</p>
-        </div>
-      </a>
-    `;
-
-    const attrs = (this.constructor as typeof RepoCard).observedAttributes;
-    attrs.forEach((attr) => {
-      let preSetValue: unknown = undefined;
-      if (Object.prototype.hasOwnProperty.call(this, attr)) {
-        preSetValue = (this as any)[attr];
-        delete (this as any)[attr];
-      }
-
-      if (!Object.getOwnPropertyDescriptor(this, attr)) {
-        Object.defineProperty(this, attr, {
-          get() {
-            if (attr === "enabled") return this.hasAttribute(attr);
-            return this.getAttribute(attr) || "";
-          },
-
-          set(val: string | boolean) {
-            if (attr === "enabled") {
-              if (val) this.setAttribute(attr, "");
-              else this.removeAttribute(attr);
-            } else {
-              this.setAttribute(attr, String(val));
-            }
-            this.render();
-          },
-
-          configurable: true,
-          enumerable: true,
-        });
-      }
-
-      if (preSetValue !== undefined) {
-        (this as any)[attr] = preSetValue;
-      }
-    });
-  }
-
-  render(): void {
-    const repo = this.getAttribute("repo") || "Repo Name";
-    const desc = this.getAttribute("desc") || "Description";
-    const stars = this.getAttribute("stars") || 0;
-    const enabled = this.getAttribute("enabled") || false;
-
-    this.innerHTML = `
-      <style>
+    const stars: number = Number(this.getAttribute("stars")) || 0;
+    const enabled = Boolean(this.getAttribute("enabled")) || false;
+    return `<style>
         .disabled-link {
           pointer-events: none;
         }
@@ -77,24 +21,71 @@ export default class RepoCard extends HTMLElement {
         </h3>
         <div class="repo-desc-stars">
           <p class="repo-desc">${desc}</p>
-          <p class="repo-stars">${stars} ${stars === "1" ? "Star" : "Stars"}</p>
+          <p class="repo-stars">${stars} ${stars === 1 ? "Star" : "Stars"}</p>
         </div>
-      </a>
-    `;
+      </a>`;
+  }
+
+  connectedCallback() {
+    this.innerHTML = this.mkInnerHTML();
+    const attrs = (this.constructor as typeof RepoCard).observedAttributes;
+    attrs.forEach((attr) => {
+      let preSetValue: unknown = undefined;
+
+      if (Object.prototype.hasOwnProperty.call(this, attr)) {
+        preSetValue = (this as any)[attr];
+        delete (this as any)[attr];
+      }
+
+      if (preSetValue !== undefined) {
+        (this as any)[attr] = preSetValue;
+      }
+
+      if (Object.getOwnPropertyDescriptor(this, attr)) {
+        return;
+      }
+
+      Object.defineProperty(this, attr, {
+        get() {
+          return attr === "enabled"
+            ? this.hasAttribute(attr)
+            : this.getAttribute(attr) || "";
+        },
+
+        set(val: string | boolean) {
+          if (attr === "enabled") {
+            if (val) this.setAttribute(attr, "");
+            else this.removeAttribute(attr);
+          } else {
+            this.setAttribute(attr, String(val));
+          }
+          this.render();
+        },
+
+        configurable: true,
+        enumerable: true,
+      });
+    });
+  }
+
+  render(): void {
+    this.innerHTML = this.mkInnerHTML();
   }
 
   attributeChangedCallback(
     name: string,
     oldValue: string | null,
-    newValue: string | null,
+    newValue: string | null
   ) {
     if (oldValue !== newValue) {
       return;
     }
+
     if (name !== "enabled") {
       const el = this.querySelector(
-        `.repo-${name === "stars" ? "stars" : name === "repo" ? "head" : "desc"}`,
+        `.repo-${name === "stars" ? "stars" : name === "repo" ? "head" : "desc"}`
       );
+
       if (el) {
         el.textContent =
           name === "stars"
